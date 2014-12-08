15bfdb6448c53130b4d71becfb1deca1634a9c21
Carlos <512.quadraphonic@gmail.com>
2014-12-08 17:54:33 -0500

    makefile for server

--- bluetooth/server.c
+++ bluetooth/server.c
@@ -7,14 +7,14 @@ int main()
 {
 	//RFCOMM structure
 	struct sockaddr_rc myAddress, remoteAddress;
-	int listenfd, client;
+	int listenfd, client, bytes_read;
 	char buf[100];
 	char str[10];
 	FILE * pFile = fopen("/etc/lux/Lux", "w");
 	listenfd = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
-	if(cc < 0){
-		perror(“RFCOMM Socket”);
-		exit(1);
+	if(listenfd < 0){
+		perror("RFCOMM Socket");
+		return 1;
 	}
 	myAddress.rc_family = AF_BLUETOOTH;
 	myAddress.rc_bdaddr = *BDADDR_ANY;
@@ -23,31 +23,30 @@ int main()
 	// listen to socket
 	listen(listenfd, 1);
 	//accept the requested connection
-	int fsize = sizeof(remoteAddress)
-	client = accept(listenfd, (struct sockaddr *)&remoteAddress, &
-	fsize);
+	int fsize = sizeof(remoteAddress);
+	client = accept(listenfd, (struct sockaddr *)&remoteAddress, &fsize);
 	memset(buf, 0, sizeof(buf));
 	/*
 	ba2str takes 6-byte remoteAddress.rc_bdaddr and copy it into buf
 	*/
-	cc = ba2str( & remoteAddress.rc_bdaddr, buf );
-	if(cc<0){
-		perror(“ba2Str”);
+	listenfd = ba2str( &remoteAddress.rc_bdaddr, buf );
+	if(listenfd<0){
+		perror("ba2Str");
 	}
-	printf(“Connected to ‘%s’\n”,buf);
+	printf("Connected to '%s'\n",buf);
 	
 	while (1){
 		memset(buf, 0, sizeof(buf));
 		// read data from the client
-		cc = read(client, buf, sizeof(buf));
+		bytes_read = read(client, buf, sizeof(buf));
 		if( bytes_read < 0 ) {
 			perror("read\n");
-			exit(1);
+			return 1;
 		}
 		fwrite(buf,1,sizeof(buf),pFile);
 		fclose(pFile);
 
-		printf(“received ‘%s’\n”,buf);
+		printf("received '%s'\n",buf);
 	}
 	// close connection
 	close(client);
